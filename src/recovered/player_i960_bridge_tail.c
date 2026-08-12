b15fdda679ab520419a91adf7d4fe453fe619160
celsowm <celsowm@gmail.com>
2026-08-12 18:36:29 -0300

    Wire native player 180bc corridor

--- src/recovered/player_i960_bridge_tail.c
+++ src/recovered/player_i960_bridge_tail.c
@@ -2,5 +2,11 @@
 #define vf2_hybrid_i960_run_tail vf2_hybrid_i960_run_tail_previous
 #include "player_i960_bridge_tail_previous.inc"
 #undef vf2_hybrid_i960_run_tail
+
 #include "player_i960_bridge_pose_data.inc"
+#define vf2_hybrid_i960_run_tail vf2_hybrid_i960_run_tail_pose
 #include "player_i960_bridge_pose_logic.inc"
+#undef vf2_hybrid_i960_run_tail
+
+/* Complete player state update immediately following the pose corridor. */
+#include "player_i960_bridge_status_logic.inc"
